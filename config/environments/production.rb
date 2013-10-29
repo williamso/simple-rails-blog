1165d45d1dd1758bea7736fbc4fae60ead9dacb4
William de Oliveira <william@criatio.com>
2013-10-29 00:34:39 -0200

    Config environments production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,10 +12,12 @@ Blog::Application.configure do
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
-  config.assets.compress = true
+  #config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compress = true
+  config.assets.compile = true
+  config.assets.precompile += %w( *.css *.js )
 
   # Generate digests for assets URLs
   config.assets.digest = true
